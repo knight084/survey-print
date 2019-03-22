3ee8be83b84d9299182294a8dbbd21076e879f8b
朱帅 <zhus@jzteyao.com>
2019-03-22 16:54:39 +0800

    测试图片打印和基于html2pdf的打印

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <!-- 需要打印的元素 -->
-    <div id="print" class="home-print">
+    <!-- 干扰元素 -->
+    <div class="home-print">
       <div class="print-title">从业人员预防性健康检查合格证明</div>
       <div class="print-content">
         <img class="content-avatar" src="../../assets/img/home/avatar.jpg">
@@ -14,14 +14,119 @@
         </ul>
       </div>
     </div>
+
+
+    <!-- 需要打印的元素 -->
+    <div class="home-print"
+         ref="print">
+      <div class="print-title">从业人员预防性健康检查合格证明</div>
+      <div class="print-content">
+        <img class="content-avatar" src="../../assets/img/home/avatar.jpg">
+        <ul class="content-list">
+          <li class="content-item"><span>姓名:</span><span>张三</span></li>
+          <li class="content-item"><span>年龄:</span><span>30</span></li>
+          <li class="content-item"><span>身份证号:</span><span>420000198901012020</span></li>
+          <li class="content-item"><span>编号:</span><span>20190322001</span></li>
+          <li class="content-item"><span>有限期止:</span><span>2020年3月22日</span></li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- 打印按钮 -->
+    <div class="home-btn">
+      <Button class="btn-img"
+              type="primary"
+              @click="handlePrintImg">打印img
+      </Button>
+      <Button class="btn-pdf"
+              type="primary"
+              @click="handlePrintPdf">打印pdf
+      </Button>
+    </div>
   </div>
 </template>
 
 <script>
+  import html2canvas from 'html2canvas';
+  import html2pdf from 'html2pdf.js';
   import printJs from 'print-js';
 
   export default {
-    name: 'Home'
+    name: 'Home',
+    methods: {
+      handlePrintImg() {
+        this.$nextTick(() => {
+          console.log('printImg');
+          const printEl = this.$refs.print;
+
+          html2canvas(printEl)
+            .then(canvas => {
+              canvas.toBlob(blob => {
+                const imgUrl = window.URL.createObjectURL(blob);
+
+                printJs({
+                  printable: imgUrl,
+                  type: 'image',
+                  logging: false,
+                  onPrintDialogClose: () => {
+                    window.URL.revokeObjectURL(imgUrl);
+
+                  },
+                  onError: e => {
+                    console.log(e);
+
+                  }
+                });
+
+
+              });
+
+            });
+
+        });
+
+      },
+      handlePrintPdf() {
+        this.$nextTick(() => {
+          console.log('printPdf');
+
+          const printEl = this.$refs.print;
+
+          html2pdf()
+            .from(printEl)
+            .outputPdf()
+            .then(bin => {
+              const blob = new Blob([bin], {type: 'application/pdf'});
+              const pdfUrl = window.URL.createObjectURL(blob);
+
+              printJs({
+                printable: pdfUrl,
+                type: 'pdf',
+                logging: false,
+                onPrintDialogClose: () => {
+                  window.URL.revokeObjectURL(pdfUrl);
+
+                },
+                onError: e => {
+                  console.log(e);
+
+                }
+              });
+
+            })
+            .catch(e => {
+              this.$message.error('失败，请重试！');
+
+              console.error(e);
+
+            });
+
+
+        });
+
+
+      }
+    }
   }
 </script>
 
@@ -37,6 +142,10 @@
       width: 280px;
       padding: 10px;
       background-color: lightblue;
+
+      &:nth-of-type(1) {
+        margin-bottom: 100px;
+      }
 
       .print-title {
         font-size: 16px;
@@ -68,6 +177,17 @@
 
         }
 
+      }
+
+    }
+
+    .home-btn {
+      width: 280px;
+      margin-top: 20px;
+      text-align: center;
+
+      .btn-pdf {
+        margin-left: 20px;
       }
 
     }
