e06344396017fef8b716cc3e8e9acf81cfa766ac
朱帅 <zhus@jzteyao.com>
2019-03-29 19:39:10 +0800

    打印华为云存储中的图片测试

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -58,8 +58,8 @@
     name: 'Home',
     data() {
       return {
-        // testImgUrl: 'https://obs-jkgl.obs.cn-north-1.myhuaweicloud.com:443/obs-tjpic/b283c11b-f8e8-4ad8-b7c0-5107cab7fedd.jpg?AccessKeyId=MTK44BF0YO9V8KGMRGF0&Expires=1553858983&Signature=INFiqP%2BreSIqlntqARoRkyeftAU%3D'
-        testImgUrl: 'http://img1.xcarimg.com/exp/2872/2875/2937/20101220130509576539.jpg'
+        testImgUrl: 'https://obs-jkgl.obs.cn-north-1.myhuaweicloud.com/obs-tjpic/b283c11b-f8e8-4ad8-b7c0-5107cab7fedd.jpg?AccessKeyId=MTK44BF0YO9V8KGMRGF0&Expires=1553862972&Signature=l2z3Vo%2BaFJe6ZLB3jySCy0TywTk%3D'
+        // testImgUrl: 'http://img1.xcarimg.com/exp/2872/2875/2937/20101220130509576539.jpg'
 
       };
 
@@ -101,7 +101,7 @@
         });
 
       },
-      handlePrintPdf() {
+      /*handlePrintPdf() {
         this.$nextTick(() => {
           console.log('printPdf');
 
@@ -138,7 +138,7 @@
 
         });
 
-      }
+      }*/
     },
     mounted() {
       const canvas = document.querySelector('#canvas');
